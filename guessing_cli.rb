d343719b42e7efae2ac3d4eb5b36958d79f4f351
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-25 21:28:51 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -6,7 +6,7 @@ puts "Guess a number between 1 and 6."
 answer = ""
 answer = gets.chomp
 answer = answer.to_i 
-computer_answer = rand(1..6).
+computer_answer = rand(1..6)
 
   if answer == "exit"
     puts "Goodbye"
@@ -15,5 +15,4 @@ computer_answer = rand(1..6).
     else
         puts "The computer guessed #{computer_answer}."
   end
-
 end
