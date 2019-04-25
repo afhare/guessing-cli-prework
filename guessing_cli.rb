6a8bada595c44bed4b4b0df38a4a2c9c02f0243d
Alex Hare <alexandra.f.hare@gmail.com>
2019-04-25 21:40:11 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -7,17 +7,22 @@ def run_guessing_game
   answer = gets.chomp
   computer_answer = rand(1..6)
 
+  if answer.downcase == "exit"
+      puts "Goodbye!"
+    end
+
   while answer != "exit"
-    if answer.to_i == computer_answer
+    elsif answer.to_i == computer_answer
       puts "You guessed the correct number!"
     elsif answer.to_i != computer_answer
         puts "The computer guessed #{computer_answer}."
     else
         puts "Invalid input!"
     end
+    
+    puts "Guess a number between 1 and 6."
+    answer = gets.chomp
+    computer_answer = rand(1..6)
   end
 
-  if answer == "exit"
-   puts "Goodbye!"
-  end
 end
